10d4bb1f7044833250dacdb431553a65ff15b310
Nemo <nemo.muller@ynov.com>
2023-09-12 19:15:13 +0200

    push

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');  // <-- Added this line
 const bodyParser = require('body-parser');
 const app = express();
 
 const port = 3000;
 
-// Utilisation de bodyParser pour traiter les données du formulaire
+// Use bodyParser to handle form data
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Définir un répertoire racine pour les fichiers statiques
+// Set a root directory for static files
 app.use(express.static(path.join(__dirname, '../')));
 
 app.get('/', (req, res) => {
-    // Lorsque quelqu'un accède à la racine, renvoyez "index.html"
+    // Serve "index.html" when someone accesses the root
     res.sendFile(path.join(__dirname, '../index.html'));
 });
 
+app.get('/api/events', (req, res) => {  // <-- Added this block
+    const jsonFilePath = path.join(__dirname, 'backend', 'events.json');
+    fs.readFile(jsonFilePath, 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).send('File not found');
+            return;
+        }
+        res.json(JSON.parse(data));
+    });
+});
 
 app.listen(port, () => console.log('Server listening on port ' + port));
