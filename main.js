29f3c899866137e866a553d21cfa95b00f9acd74
Nemo <nemo.muller@ynov.com>
2023-09-12 11:08:30 +0200

    Push reset

--- main.js
+++ main.js
@@ -1,29 +1,23 @@
 class Player {
     constructor(username, alignment, gold, inventory) {
-        this.username = username;
-        this.alignment = alignment;
-        this.gold = gold;
-        this.inventory = inventory;
+      this.username = username;
+      this.alignment = alignment;
+      this.gold = gold;
+      this.inventory = inventory;
     }
 }
 
-// Fonction pour initialiser le joueur
 function initializePlayer() {
     const username = prompt("Entrez votre pseudo :");
     const alignment = parseInt(prompt("Entrez votre niveau d'alignement :"));
     const gold = parseInt(prompt("Entrez la quantité d'or que vous possédez :"));
-    
-    // Créez une instance de la classe Player avec les valeurs entrées
-    const player = new Player(username, alignment, gold, []);
-
-    // Redirigez vers la page d'initialisation avec les paramètres du joueur
-    window.location.href = `initialisation.html?username=${player.username}&alignment=${player.alignment}&gold=${player.gold}`;
+}
+function initializePlayer() {
+    const username = prompt("Entrez votre pseudo :");
+    const alignment = parseInt(prompt("Entrez votre niveau d'alignement :"));
+    const gold = parseInt(prompt("Entrez la quantité d'or que vous possédez :"));
 }
 
-<<<<<<< HEAD
-// Appelez la fonction initializePlayer pour initialiser le joueur et déclencher la redirection
-initializePlayer();
-=======
 window.location.href = `initialisation.html?username=${player.username}&alignment=${player.alignment}&gold=${player.gold}`;
 
 const player = new Player(username, alignment, gold, []);
@@ -67,5 +61,3 @@ let item = {
     effect: "",
 
 };
-
->>>>>>> 35b51363765a13461422f07c48ee073d5b801a46
