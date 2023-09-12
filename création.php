117e44eb6097f021b66611c575db2cf6effa9785
DYAHIABERROUIGUET <95360745+DYAHIABERROUIGUET@users.noreply.github.com>
2023-09-12 13:26:00 +0200

    change

--- création.php
+++ création.php
@@ -1,22 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
 
     <label>Comment vous appelez-vous ?
         <input type='text' name='firstName' id='firstName' placeholder="First Name">
     </label>
-    
+
     <div class="titre">
         <h3>Veuillez choisir un objet pour commencer l'aventure</h3>
     </div>
 
     <div class="objet">
-        
+
         <input type="checkbox" class="single-checkbox" id="" name="" value="objet1">
         <label for="objet1">Trèfle à quatre feuilles</label><br>
         <input type="checkbox" class="single-checkbox" id="" name="" value="objet2">
@@ -27,36 +29,41 @@
     </div>
     <p>Questionnaire d'alignement :</p>
     <label>
-      <input type="radio" name="alignment" value="good"> Bonne action (+20 d'alignement)
+        <input type="radio" name="alignment" value="good"> Bonne action (+20 d'alignement)
     </label><br>
     <label>
-      <input type="radio" name="alignment" value="neutral" checked> Neutre (0 d'alignement)
+        <input type="radio" name="alignment" value="neutral" checked> Neutre (0 d'alignement)
     </label><br>
     <label>
-      <input type="radio" name="alignment" value="bad"> Mauvaise action (-20 d'alignement)
+        <input type="radio" name="alignment" value="bad"> Mauvaise action (-20 d'alignement)
     </label><br>
 
-    <button onclick="window.location='initialisation.html';"  id="start">Créer le joueur</button>
-   
-     
-  </form>
+    <button onclick="window.location='initialisation.html';" id="start">Créer le joueur</button>
+
+
+    </form>
 
 </body>
-    <script>
-        const checkboxes = document.querySelectorAll('.single-checkbox');
+<script>
+    const checkboxes = document.querySelectorAll('.single-checkbox');
 
-        // Ajoutez un écouteur d'événements à chaque case à cocher
-        checkboxes.forEach(function(checkbox) {
+    // Ajoutez un écouteur d'événements à chaque case à cocher
+    checkboxes.forEach(function(checkbox) {
         checkbox.addEventListener('change', function(e) {
             // Si la case à cocher est sélectionnée, désélectionnez toutes les autres
             if (e.target.checked) {
-            checkboxes.forEach(function(box) {
-                if (box !== e.target) {
-                box.checked = false;
-                }
-            });
+                checkboxes.forEach(function(box) {
+                    if (box !== e.target) {
+                        box.checked = false;
+                    }
+                });
             }
         });
-        });
-    </script>
+    });
+</script>
+<?php
+$dsn = 'mysql:host=127.0.0.1;dbname=formulaireprojectblue;charset=utf8mb4';
+$pdo = new PDO($dsn, 'root', '');
+?>
+
 </html>
