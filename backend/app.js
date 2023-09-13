a13aed01a1fbcfb5326e1a59a2c7c98807df667b
djilali yahia <DYAHIABERROUIGUET>
2023-09-13 11:46:01 +0200

    challah

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,22 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');  // <-- Added this line
+const fs = require('fs'); 
 const bodyParser = require('body-parser');
 const app = express();
 
 const port = 3000;
 
-// Use bodyParser to handle form data
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json())
 
-// Set a root directory for static files
 app.use(express.static(path.join(__dirname, '../')));
 
 app.get('/', (req, res) => {
-    // Serve "index.html" when someone accesses the root
     res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-app.get('/api/events', (req, res) => {  // <-- Added this block
+app.get('/api/events', (req, res) => { 
     const jsonFilePath = path.join(__dirname, 'backend', 'events.json');
     fs.readFile(jsonFilePath, 'utf8', (err, data) => {
         if (err) {
