2ce13702c5187a118b3ce444f2b38a4a9b60a645
Nemo <nemo.muller@ynov.com>
2023-09-14 11:06:32 +0200

    Push

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs'); 
+const fs = require('fs');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
@@ -13,11 +13,13 @@ app.use(express.json())
 
 app.use(express.static(path.join(__dirname, '../')));
 
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-app.get('/api/events', (req, res) => { 
+
+app.get('/api/events', (req, res) => {
     const jsonFilePath = path.join(__dirname, 'backend', 'events.json');
     fs.readFile(jsonFilePath, 'utf8', (err, data) => {
         if (err) {
@@ -27,60 +29,76 @@ app.get('/api/events', (req, res) => {
         res.json(JSON.parse(data));
     });
 });
-    app.get("/test/:id", (req, res) => {
-        console.log(req.params);
-        res.send("i love ynov test get"); 
-    });
-
-    app.put("/update/alignement",(req, res) => {
 
 
+app.get("/test/:id", (req, res) => {
+    console.log(req.params);
+    res.send("i love ynov test get");
+});
 
-    })
 
-    app.post("/test", (req, res) => {
-        console.log(req.body);
-        const filePath = "data.json";
-        // preparation data
-        
-        let TotalAlignment = parseInt(req.body.alignmentQuestion1) + parseInt(req.body.alignmentQuestion2) + parseInt(req.body.alignmentQuestion3)
-        jsonData = {
-            "username" : req.body.username,
-            "startingItem" : "Inventaire : "+req.body.startingItem,
-            "alignment" : TotalAlignment,
-            "argent": "Argent : "+100+"$",
-            "inventory": []
-        };
+// a faire 
+app.post("/event/data/:id", (req, res) => {
+    const filePath = "data.json";
+    console.log(req.body)
+    TotalAlignment = data.alignment- 20 
+    jsonData = {
+        "username": req.body.username,
+        "startingItem": "Inventaire : " + req.body.startingItem,
+        "alignment": TotalAlignment,
+        "argent": "Argent : " + 100 + "$",
+        "inventory": []
+    };
+    fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2));
+})
+
+app.post("/test", (req, res) => {
+    console.log(req.body);
+    const filePath = "data.json";
+    // preparation data
+
+    let TotalAlignment = parseInt(req.body.alignmentQuestion1) + parseInt(req.body.alignmentQuestion2) + parseInt(req.body.alignmentQuestion3)
+    jsonData = {
+        "username": req.body.username,
+        "startingItem": "Inventaire : " + req.body.startingItem,
+        "alignment": TotalAlignment,
+        "argent": "Argent : " + 100 + "$",
+        "inventory": []
+    };
     fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2));
 
     res.redirect("question.html")
-    });
-    
-    // Votre route pour afficher l'événement et gérer la réponse
-    app.get('/events/:id', (req, res) => {
-        const id = parseInt(req.params.id);
-        const events = data.events;
-        const event = events.find(s => s.id === id);
-    
-        if (event) {
-            const choixA = event.choix.reponse.find(choix => choix.a === 'a');
-            const choixB = event.choix.reponse.find(choix => choix.b === 'b');
-            
+});
 
-            const htmlResponse = `
+// Votre route pour afficher l'événement et gérer la réponse
+app.get('/events/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const events = data.events;
+    const event = events.find(s => s.id === id);
+
+    if (event) {
+        const choixA = event.choix.reponse.find(choix => choix.a === 'a');
+        const choixB = event.choix.reponse.find(choix => choix.b === 'b');
+
+
+        const htmlResponse = `
+                <form action="http://localhost:3000/event/data/${id}" method="POST">
                 <h2>Description de l'événement</h2>
                 <p>${event.description}</p>
-                <input type="radio" id="choixA" name="choix" value="a">
+                <input type="radio" id="choixA" name="choix" value="${choixA.effect}">
                 <label for="choixA">${choixA.choix_a}</label>
     
-                <input type="radio" id="choixB" name="choix" value="b">
+                <input type="radio" id="choixB" name="choix" value="${choixB.effect}">
                 <label for="choixB">${choixB.choix_b}</label>
-                `;
-    
-            res.send(htmlResponse);
-        } else {
-            res.status(404).send('Événement non trouvé');
-        }
-    });
+                <button id="fetchButton" type="submit">Afficher Événement Suivant</button>
+                </form>`;
+
+        res.send(htmlResponse);
+    } else {
+        res.status(404).send('Événement non trouvé');
+    }
+});
+
 
 app.listen(port, () => console.log('Server listening on port ' + port));
+
